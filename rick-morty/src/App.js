6806335dc81bd18bf8715b97b30ea42db741e87e
Gilberto <gilberto@teknik.mx>
2025-05-24 22:36:11 -0600

    request in API

---
+++ rick-morty/src/App.js
@@ -0,0 +1,25 @@
+import imageRickMorty from './img/rick-morty.png';
+import './App.css';
+
+function App() {
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    
+    console.log(characterApi);
+    
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
+        <button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
