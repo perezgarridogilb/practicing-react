dd0e2736015b4bb919daff26dfc36da2cafe13d8
Gilberto <gilberto@teknik.mx>
2025-05-24 22:48:17 -0600

    creating component from characters

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,20 +1,27 @@
 import imageRickMorty from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
+import Character from './components/Characters';
 
 function App() {
+  const [character, setCharacters] = useState(null);
 
   const reqApi = async () => {
     const api = await fetch("https://rickandmortyapi.com/api/character");
     const characterApi = await api.json();
     
-    console.log(characterApi);
+    setCharacters(characterApi);
     
   }
+
+ console.log(character);
+ 
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
+        <Character character={character}/>
         <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
         <button onClick={reqApi} className='btn-search'>Buscar personajes</button>
       </header>
---
+++ rick-morty/src/components/Characters.js
@@ -0,0 +1,9 @@
+export default function character(props) {
+    console.log(props);
+    
+    return (
+        <div>
+            <h1>Desde characters JS</h1>
+        </div>
+    )
+}
