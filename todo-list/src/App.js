f0f50c0c0e0858afc65249d78074559d72a43b2c
Gilberto <gilberto@teknik.mx>
2025-06-17 23:13:16 -0600

    Creating createTask() function

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -5,12 +5,36 @@ import { Button, Icon } from 'semantic-ui-react';
 import Container from './components/Container';
 import Header from './components/Header';
 import InputTask from './components/InputTask';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  let initialTasks = JSON.parse(localStorage.getItem('tasks'));
+  
+  if (!initialTasks) {
+    initialTasks = [];
+  }
+
+  const [tasks, setTasks] = useState(initialTasks);
+
+  useEffect(() => {
+    if (initialTasks) {
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      localStorage.setItem('tasks', JSON.stringify([]));
+    }
+  }, [initialTasks, tasks]);
+    
+  const createTask = (task) => {
+    setTasks([...tasks, task]);
+    
+  };
+  console.log(tasks);
+
   return (
     <Container>
       <Header />
-      <InputTask></InputTask>
+      <InputTask createTask={createTask}></InputTask>
     </Container>
   );
 }
--- todo-list/src/components/InputTask.js
+++ todo-list/src/components/InputTask.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Select, Input, Button, Grid, Header, Icon } from "semantic-ui-react";
+import { v4 as uuidv4 } from 'uuid';
 
 const options = [
     { key: "deporte", text: "Deporte", value: "deporte" },
@@ -7,7 +9,67 @@ const options = [
     { key: "otra", text: "Otra", value: "otra" },
 ]
 
-export default function InputTask() {
+
+export default function InputTask(props) {
+    const [task, setTask] = useState({
+        idTask: "",
+        taskName: "",
+        categoryTask: ""
+    });
+
+    const onChangeTask = (e, data) => {
+        console.log("Tocando onChangeTask");
+        debugger;
+        setTask({
+            ...task,
+            [e.target.name]: e.target.value
+        })
+        
+    };
+
+    const onChangeCategoryTask = (e, data) => {
+        
+        setTask({
+            ...task,
+            [data.name]: data.value,
+        })
+        console.log(task);
+    };
+
+    const onSubmitTask = (e) => {
+        // que no recargue la página
+        e.preventDefault();
+        console.log('enviando tarea');
+
+        // validación
+        if (task.taskName.trim() === '' && task.categoryTask.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        // eliminar el msj previo
+        setError(false);
+
+        // asignar un ID
+        const id = uuidv4();
+        task.idTask = id;
+        console.log(id);
+
+        // crear la tarea
+        createTask(task);
+
+        // limpiar los inputs
+        setTask({
+            idTask: "",
+            taskName: "",
+            categoryTask: ""
+        })
+    }
+    
+    const [error, setError] = useState(false);
+
+    const { createTask } = props;
+
     return (<>
         <Grid centered columns={2}>
             <Input type="small"
@@ -15,6 +77,8 @@ export default function InputTask() {
                 placeholder="Escribe tu tarea..."
                 iconPosition="left"
                 name="taskName"
+                value={task.taskName}
+                onChange={onChangeTask}
             />
             <Select
                 compact
@@ -22,11 +86,22 @@ export default function InputTask() {
                 className="select-form-task"
                 name="categoryTask"
                 placeholder="Categoria"
+                value={task.categoryTask}
+                onChange={onChangeCategoryTask}
             />
-            <Button type="submit" color="violet">
+            <Button type="submit" color="violet" onClick={onSubmitTask}>
                 Añadir
             </Button>
         </Grid>
+        {error && (
+            <Grid centered>
+                <Header as="h4" color="red" className="alert-error-form">
+                    <Icon name="close" />
+                    <Header.Content>La tarea es obligatoria</Header.Content>
+                    <Icon name="close" />
+                </Header>
+            </Grid>
+        )}
     </>
     );
 };
